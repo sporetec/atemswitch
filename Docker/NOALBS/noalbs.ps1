1a525a705226d73f8e1fbcde67a8370e9a0363f7
SporeTec <mail@sporetec.at>
2023-04-11 00:26:05 +0200

    added force remove of docker container with silenced output

--- Docker/NOALBS/noalbs.ps1
+++ Docker/NOALBS/noalbs.ps1
@@ -1,1 +1,2 @@
-docker run -d --name=noalbs --restart=unless-stopped --mount src=$PWD/config,target=/home/noalbs_files,type=bind local/noalbs
+docker rm -f noalbs | out-null
+docker run -it --name=noalbs --restart=unless-stopped --mount src=$PWD/config,target=/home/noalbs_files,type=bind local/noalbs
--- Docker/srtServer/srt.ps1
+++ Docker/srtServer/srt.ps1
@@ -1,1 +1,2 @@
-docker run -d -p 30000:30000/udp -p 8181:8181/tcp --name=sls-b3ck-edit --restart=unless-stopped --pull=always -v sls-b3ck-edit_data:/data b3ckontwitch/sls-b3ck-edit
+docker rm -f srt-server | out-null
+docker run -d -p 30000:30000/udp -p 8181:8181/tcp --name=srt-server --restart=unless-stopped --pull=always -v sls-b3ck-edit_data:/data b3ckontwitch/sls-b3ck-edit
